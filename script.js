2315041f99b8aa414ae0bc2f7fcd3a001b593b35
carolcortes <caroline.ocortes@gmail.com>
2022-04-25 22:31:13 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log('Olá, mundo!');
+
+// create list
+const input = document.getElementById('texto-tarefa');
+const oList = document.getElementById('lista-tarefas');
+const addButton = document.getElementById('criar-tarefa');
+function createList() {
+  const tarefa = document.createElement('li');
+  tarefa.innerText = input.value;
+  oList.appendChild(tarefa);
+  input.value = '';
+}
+addButton.addEventListener('click', createList)
+
+window.onload = () => {
+
+};
