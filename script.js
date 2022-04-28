405354b50117eb4bba11dbe74b66cd7b37b810a1
carolcortes <caroline.ocortes@gmail.com>
2022-04-27 22:08:48 -0300

    requisito 8

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 console.log('Olá, mundo!');
 
+// select item list
+function selectItem(event) {
+  const myEvent = event;
+  const tarefaSelecionada = document.getElementsByClassName('tarefa selecionada');
+  if (tarefaSelecionada.length >= 1) {
+    tarefaSelecionada[0].classList.remove('selecionada');
+  }
+  myEvent.target.classList.add('selecionada');
+}
+
 // create list
 const input = document.getElementById('texto-tarefa');
 const oList = document.getElementById('lista-tarefas');
@@ -7,12 +17,12 @@ const addButton = document.getElementById('criar-tarefa');
 function createList() {
   const tarefa = document.createElement('li');
   tarefa.innerText = input.value;
-  tarefa.className = 'tarefa';
   oList.appendChild(tarefa);
+  tarefa.className = 'tarefa';
+  tarefa.addEventListener('click', selectItem);
   input.value = '';
 }
 addButton.addEventListener('click', createList);
 
 window.onload = () => {
-
 };
